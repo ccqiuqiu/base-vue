3d142a358bdc19fe47d1a7805bf8403cfdb7cd6d
cc <cc--x@163.com>
2017-12-28 11:21:23 +0800

    修改前进后退动画

--- src/global/router.js
+++ src/global/router.js
@@ -24,14 +24,16 @@ const router = new Router({
 // 通过路由的变化，判断是前进还是后退，将后退标识存入路由元数据
 const routerList = []
 router.beforeEach((to, from, next) => {
-  if (routerList.length && routerList.indexOf(to.name) === routerList.length - 1) {
+  if (routerList.length && routerList.lastIndexOf(to.path) === routerList.length - 1) {
     // 后退
     routerList.splice(routerList.length - 1, 1)
     to.meta.isBack = true
+    from.meta.isBack = true
   } else {
     // 前进
-    routerList.push(from.name || '/')
+    routerList.push(from.path || '/')
     to.meta.isBack = false
+    from.meta.isBack = false
   }
   next()
 })
