f2908e974cb1f1cbed45db2b24f79fd30bcf08e0
cc <cc--x@163.com>
2018-01-19 15:45:11 +0800

    -修改一些样式

--- src/modules/common/components/CitySelect.vue
+++ src/modules/common/components/CitySelect.vue
@@ -1,6 +1,6 @@
 <!--Created by 熊超超 on 2017/12/9.-->
 <template>
-  <air-index-select
+  <air-index-select class="self-scroll"
     :data="data"
     :hots="hots"
     text-field="citynameCN"
@@ -10,7 +10,7 @@
     :search-fields="searchFields"
     :tabs="tabs"
     key-field="itatCode"
-    @on-selected="selected" />
+    @on-selected="selected"/>
 </template>
 
 <script>
